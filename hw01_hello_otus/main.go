96f034212205b111eeba9771761a22afd486e770
dmitrykharchenko95 <dmitry.kharchenko95@gmail.com>
2021-09-06 15:00:29 +0300

    fix: correct module path

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"github.com/dmitrykharchenko95/hw/hw01_hello_otus/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	fmt.Println(stringutil.Reverse("Hello, OTUS!"))
 }
