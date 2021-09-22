3d83cd254652a92e91f1d106f84d8ad33ebbf5ac
dmitrykharchenko95 <dmitry.kharchenko95@gmail.com>
2021-09-22 17:25:24 +0300

    fix: strings.Fields -> strings.FieldsFunc; slice -> map for count value. remove: else, debug-print

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -21,42 +21,31 @@ func Top10(inputString string) []string {
 		return !unicode.IsLetter(r) && !unicode.IsNumber(r) && r != '-'
 	})
 
-	sort.Slice(inputSlice, func(i, j int) bool {
-		return inputSlice[i] < inputSlice[j]
-	})
+	inputMap := make(map[string]int)
 
-	amountData := []StringAndAmount{}
-	amount := 1
-
-	for i := 0; i < len(inputSlice); {
-		switch {
-		case i == len(inputSlice)-1:
-			amountData = append(amountData, StringAndAmount{inputSlice[i], amount})
-			i++
-		case inputSlice[i] == inputSlice[i+1]:
-			i++
-			amount++
-			continue
-		case inputSlice[i] != inputSlice[i+1]:
-			amountData = append(amountData, StringAndAmount{inputSlice[i], amount})
-			i++
-			amount = 1
-		}
+	for _, v := range inputSlice {
+		inputMap[v]++
 	}
-	sort.Slice(amountData, func(i, j int) bool {
-		if amountData[i].Amount != amountData[j].Amount {
-			return amountData[i].Amount > amountData[j].Amount
+
+	fullOutputSlice := []StringAndAmount{}
+	for key, val := range inputMap {
+		fullOutputSlice = append(fullOutputSlice, StringAndAmount{key, val})
+	}
+
+	sort.Slice(fullOutputSlice, func(i, j int) bool {
+		if fullOutputSlice[i].Amount != fullOutputSlice[j].Amount {
+			return fullOutputSlice[i].Amount > fullOutputSlice[j].Amount
 		}
-		return amountData[i].String < amountData[j].String
+		return fullOutputSlice[i].String < fullOutputSlice[j].String
 	})
 
 	var outputSlice []string
 	num := 10
-	if len(amountData) < 10 {
-		num = len(amountData)
+	if len(fullOutputSlice) < 10 {
+		num = len(fullOutputSlice)
 	}
 	for i := 0; i < num; i++ {
-		outputSlice = append(outputSlice, amountData[i].String)
+		outputSlice = append(outputSlice, fullOutputSlice[i].String)
 	}
 
 	return outputSlice
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,9 +17,71 @@ type ListItem struct {
 }
 
 type list struct {
-	List// Remove me after realization.
-	ListItem
+	//List// Remove me after realization.
+	Data 		[]ListItem
+	FrontItem 	ListItem
+	BackItem 	ListItem
 	// Place your code here.
 }
 
+func NewList() List {
+	return new(list)
+}
+
+func (l list) Len() int {
+	return len(l.Data)
+}
+
+func (l list) Front() *ListItem  {
+	for _, f := range l.Data {
+		if f.Prev == nil {
+			return &f
+		}
+		f = *f.Prev
+	}
+	return nil
+}
+
+func (l list) Back() *ListItem {
+	for _, b := range l.Data {
+		if b.Next == nil {
+			return &b
+		}
+		b = *b.Next
+	}
+	return nil
+}
+
+func (l list) PushFront (v interface{}) *ListItem  {
+	f := ListItem {
+		v,
+		l.Front(),
+		nil,
+	}
+	l.Front().Prev = &f
+	return &f
+}
+
+func (l list) PushBack (v interface{}) *ListItem  {
+	b := ListItem {
+		v,
+		nil,
+		l.Back(),
+	}
+	l.Back().Next = &b
+	return &b
+}
+
+func (l list) Remove(i *ListItem)  {
+	i.Prev.Next = i.Next
+	i.Next.Prev = i.Prev
+	i.Next, i.Prev = nil, nil
+}
+
+func (l list) MoveToFront(i *ListItem)()  {
+	i.Prev = nil
+	i.Next = l.Front()
+	i.Prev.Next = i.Next
+	i.Next.Prev = i.Prev
+}
 
